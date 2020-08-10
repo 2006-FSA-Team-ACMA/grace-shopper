1a0a3902296bb3625bb69a5fe8b8f105af03a585
Adam Sue <adam.p.sue@gmail.com>
2020-08-10 18:10:05 -0400

    added back end and redux functionality for user cart

--- client/store/userCart.js
+++ client/store/userCart.js
@@ -1,15 +1,12 @@
-// import axios from 'axios'
+import axios from 'axios'
 // import history from '../history'
 
 // ACTION TYPE
 const GET_USER_CART = 'GET_USER_CART'
-const ADD_TO_USER_CART = 'ADD_TO_USER_CART'
+// const ADD_TO_USER_CART = 'ADD_TO_USER_CART'
 
 //this is userCart
-export const initialState = {
-  addedProducts: [],
-  total: 0
-}
+export const initialState = []
 
 // ACTION CREATOR
 
@@ -20,10 +17,29 @@ export const getUserCart = cart => {
   }
 }
 
-export const addToUserCart = product => {
-  return {
-    type: ADD_TO_USER_CART,
-    product
+// export const addToUserCart = (product) => {
+//   return {
+//     type: ADD_TO_USER_CART,
+//     product,
+//   }
+// }
+
+// FETCHES USER'S COMPLETE ORDERS
+export const fetchUserCart = userId => async dispatch => {
+  try {
+    const {data: orders} = await axios.get(`/api/users/${userId}/orders`)
+    dispatch(getUserCart(orders))
+  } catch (error) {
+    console(error)
+  }
+}
+
+export const fetchAddToCart = (product, userId) => async dispatch => {
+  try {
+    await axios.post(`/api/users/${userId}/orders`, product)
+    dispatch(fetchUserCart(userId))
+  } catch (error) {
+    console(error)
   }
 }
 
@@ -31,30 +47,28 @@ export const addToUserCart = product => {
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_USER_CART:
-      return state.cart
-    case ADD_TO_USER_CART:
-      console.log('add to user cart action', action)
-      let existedProduct = state.addedProducts.find(
-        product => action.product.id === product.id
-      )
-      if (existedProduct) {
-        action.product.quantity += 1
-        // console.log('ADDED ITEMS[]', state.addedProducts)
-        console.log('STATE TOTAL>>>', state.total)
-        return {...state, total: state.total + action.product.price}
-      } else {
-        action.product.quantity = 1
-        let newTotal = state.total + action.product.price
-        // console.log('TOTAL>>>', state.total)
+      return action.cart
+    // case ADD_TO_USER_CART:
+    // newState.push(action.product)
+    // let existedProduct = state.addedProducts.find(
+    //   (product) => action.product.id === product.id
+    // )
+    // if (existedProduct) {
+    //   action.product.quantity += 1
+    //   console.log('STATE TOTAL>>>', state.total)
+    //   return {...state, total: state.total + action.product.price}
+    // } else {
+    //   action.product.quantity = 1
+    //   let newTotal = state.total + action.product.price
 
-        const result = {
-          ...state,
-          addedProducts: [...state.addedProducts, action.product],
-          total: newTotal
-        }
-        console.log('RESULT>>>', result)
-        return result
-      }
+    //   const result = {
+    //     ...state,
+    //     addedProducts: [...state.addedProducts, action.product],
+    //     total: newTotal,
+    //   }
+    //   console.log('RESULT>>>', result)
+    //   return result
+    // }
     default:
       return state
   }
